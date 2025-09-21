9769417116de7b6b64bf4a593206f8ab5ca403d5
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-09-21 13:00:32 +0530

    Dark mode Toggle

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
       </button>
 
       {/* Search Conversations */}
-      <div className="flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md">
+      <div className="flex items-center gap-2 p-3 mt-2 border border-gray-400 dark:border-white/20 rounded-md">
         <img src={assets.search_icon} className="w-4 not-dark:invert" alt="" />
         <input
           onChange={(e) => setSearch(e.target.value)}
@@ -36,7 +36,7 @@ const Sidebar = () => {
       </div>
 
       {/* Recent Chats */}
-      {chats.length > 0 && <p className="mt-4 mb-2 text-sm ">Recent Chats</p>}
+      {chats.length > 0 && <p className="mt-4 mb-1 text-sm ">Recent Chats</p>}
       <div>
         {chats
           .filter((chat) =>
@@ -49,7 +49,7 @@ const Sidebar = () => {
           .map((chat) => (
             <div
               key={chat._id}
-              className="mb-2 p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
+              className="mb-1 p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
             >
               <div>
                 <p className="truncate w-full">
@@ -73,38 +73,53 @@ const Sidebar = () => {
       {/* Community Images */}
       <div
         onClick={() => {
-          navigate('/community')
+          navigate("/community");
         }}
-        className='flex items-center gap-2 p-3 mt-38 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'
+        className="flex items-center gap-2 p-3 mt-32 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
         <img
           src={assets.gallery_icon}
-          className='w-4.5 not-dark:invert'
+          className="w-4.5 not-dark:invert"
           alt=""
         />
-        <div className='flex flex-col text-sm'>
+        <div className="flex flex-col text-sm">
           <p>Community Images</p>
         </div>
       </div>
 
-       {/* Creadits purchases Option */}
+      {/* Creadits purchases Option */}
       <div
         onClick={() => {
-          navigate('/community')
+          navigate("/community");
         }}
-        className='flex items-center gap-2 p-3 mt-auto border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'
+        className="flex items-center gap-2 p-3 mt-1 mb-1 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
-        <img
-          src={assets.diamond_icon}
-          className='w-4.5 dark:invert'
-          alt=""
-        />
-        <div className='flex flex-col text-sm'>
+        <img src={assets.diamond_icon} className="w-4.5 dark:invert" alt="" />
+        <div className="flex flex-col text-sm">
           <p>Credits : {user?.credits}</p>
-          <p className='text-xs text-gray-400'>purchase credits to use quickgpt</p>
+          <p className="text-xs text-gray-400">
+            purchase credits to use quickgpt
+          </p>
         </div>
       </div>
 
+      {/* Dark mode Toggle */}
+      <div className="flex items-center justify-between gap-2 p-3 mt-auto border border-gray-300 dark:border-white/15 rounded-md ">
+        <div className="flex items-center gap-2 text-sm">
+          <img src={assets.theme_icon} className="w-4 not-dark:invert" alt="" />
+          <p>Dark Mode</p>
+        </div>
+        <label className='relative inline-flex cursor-pointer'>
+          <input
+            onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+            type="checkbox"
+            className="sr-only peer"
+            checked={theme === "dark"}
+          />
+          <div className="w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all"></div>
+          <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4"></span>
+        </label>
+      </div>
     </div>
   );
 };
