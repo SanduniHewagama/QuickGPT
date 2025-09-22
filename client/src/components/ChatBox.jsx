ecceabc340f03b4433265a190361eb059edce43f
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-09-22 15:51:35 +0530

    add checkbox

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -9,6 +9,13 @@ const ChatBox = () => {
   const [messages, setMessages] = useState([]);
   // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(false);
+  const [prompt, setPrompt] = useState("");
+  const [mode, setMode] = useState("text");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
 
   useEffect(() => {
     if (selectedChat) {
@@ -17,9 +24,9 @@ const ChatBox = () => {
   }, [selectedChat]);
 
   return (
-    <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
+    <div className="flex-1 flex flex-col justify-between md:m-10 xl:mx-30 max-md:mt-auto 2xl:pr-40">
       {/* Chat Messages */}
-      <div className="flex-1 mb-5 overflow-y-scroll">
+      <div className="flex-1 mb-2 ">
         {messages.length === 0 && (
           <div className="h-full flex flex-col items-center justify-center gap-2 text-primary">
             <img
@@ -46,9 +53,52 @@ const ChatBox = () => {
           </div>
         )}
 
-        {/* Pompt Input BOx */}
-        <form></form>
+        {mode === "image" && (
+          <label className="inline-flex items-center gap-2 mb-auto text-sm mx-32">
+            <p className="text-xs">Publish Generated Image to Community</p>
+            <input
+              type="checkbox"
+              className="cursor-pointer"
+              checked={isPublished}
+              onChange={(e) => setIsPublished(e.target.checked)}
+            />
+          </label>
+        )}
       </div>
+
+      {/* Pompt Input BOx */}
+      <form
+        onSubmit={onSubmit}
+        className="bg-primary/20 dark:bg-[#583C79]/30 border border-primary dark:border-[#80609F]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center"
+      >
+        <select
+          onChange={(e) => setMode(e.target.value)}
+          value={mode}
+          className="text-sm pl-3 pr-2 outline-none"
+        >
+          <option className="dark:bg-purple-900" value="text">
+            Text
+          </option>
+          <option className="dark-bg-purple-900" value="image">
+            Image
+          </option>
+        </select>
+        <input
+          onChange={() => setPrompt(e.target.value)}
+          value={prompt}
+          type="text"
+          placeholder="Type your prompt here..."
+          className="flex-1 w-full text-sm outline-none"
+          required
+        />
+        <button>
+          <img
+            src={loading ? assets.stop_icon : assets.send_icon}
+            className="w-8 cursor-pointer"
+            alt=""
+          />
+        </button>
+      </form>
     </div>
   );
 };
