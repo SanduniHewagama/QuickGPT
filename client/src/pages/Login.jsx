c4277c7194aa647cb04613bfaceabaa1b9ddac9e
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-09-22 18:21:48 +0530

    fix errors

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+// import { useState } from "react";
 
 const Login = () => {
   const [state, setState] = React.useState("login");
