87687a52d5c09d91d0b43bc9730394002877116d
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-09-23 17:23:29 +0530

    errors fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
@@ -12,7 +13,7 @@ userSchema.pre('save', async function(next) {
     if(!this.isModified('password')){
         return next()
     }
-    const salt = await bcrypt.getSalt(10)
+    const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
     next();
     
