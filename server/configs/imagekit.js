7f2a86b0a88ea5054ea010a614e4754aea77f273
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-10-06 00:23:26 +0530

    messgaes routes

---
+++ server/configs/imagekit.js
@@ -0,0 +1,15 @@
+import ImageKit from '@imagekit/nodejs';
+
+
+// const client = new ImageKit({
+//   privateKey: process.env['IMAGEKIT_PRIVATE_KEY'], // This is the default and can be omitted
+// });
+
+var imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+export default imagekit
