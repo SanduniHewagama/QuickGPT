94b9165aa7a172a37292f861a8cb57e4fef9a9af
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-10-07 09:49:49 +0530

    values assign for chatbox

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -6,7 +6,7 @@ import Message from "./Message";
 const ChatBox = () => {
 
   const containerRef = useRef(null)
-  const { selectedChat, theme } = useAppContext();
+  const { selectedChat, theme, user, axios, token, setUser } = useAppContext();
   const [messages, setMessages] = useState([]);
   // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(false);
